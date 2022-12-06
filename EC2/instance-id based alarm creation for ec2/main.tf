31bc1a478313d92ba35c0fccf93682db45c1565f
Nishachinnannan <100110248+Nishachinnannan@users.noreply.github.com>
2022-12-06 09:53:17 +0530

    Create main.tf

---
+++ EC2/instance-id based alarm creation for ec2/main.tf
@@ -0,0 +1,20 @@
+provider "aws" {
+region = "eu-west-1"
+}
+############### create Cloudwatch alarm ##########################
+resource "aws_cloudwatch_metric_alarm" "test" {
+  alarm_name         = format("%s-%s",var.instance,"CPU-UTILIZATION-HIGH")
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "180"
+  statistic           = "Average"
+  threshold           = "90"
+ alarm_actions        = [var.sns_topic_name]
+
+  dimensions = {
+    InstanceId = "${var.instance}"
+    TopicName = "${var.sns_topic_name}"
+  }
+ }
