53f4ca7b7956900f69c4f23a03d32a234c9b2671
Nishachinnannan <100110248+Nishachinnannan@users.noreply.github.com>
2022-12-14 22:09:08 +0530

    Update main.tf

--- EC2/Tag-based-ec2-alarm/main.tf
+++ EC2/Tag-based-ec2-alarm/main.tf
@@ -28,3 +28,34 @@ resource "aws_cloudwatch_metric_alarm" "bat" {
      InstanceId = data.aws_instance.selected.id
   }
  }
+resource "aws_cloudwatch_metric_alarm" "CpuUtilization1" {
+  alarm_name          = format("%s-%s", "${data.aws_instance.selected.id}", "Warning-CPU-Utilizaton")
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+ alarm_actions        = [var.sns_topic_name]
+
+  dimensions = {
+     InstanceId = data.aws_instance.selected.id
+  }
+ }
+resource "aws_cloudwatch_metric_alarm" "instance-health-check" {
+ alarm_name          = format("%s-%s", "${data.aws_instance.selected.id}", "StatusCheckFailed_System")
+comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "StatusCheckFailed_System"
+  namespace           = "AWS/EC2"
+  period              = "120"
+  statistic           = "Average"
+  threshold           = "1"
+  alarm_actions        = [var.sns_topic_name]
+
+  dimensions = {
+     InstanceId = data.aws_instance.selected.id
+  }
+ }
+
