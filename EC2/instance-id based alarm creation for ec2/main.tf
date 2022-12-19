caca1222cabe8df58c6be11c76c91e05ef8d3b76
Nishachinnannan <100110248+Nishachinnannan@users.noreply.github.com>
2022-12-19 08:55:38 +0530

    Update main.tf

--- EC2/instance-id based alarm creation for ec2/main.tf
+++ EC2/instance-id based alarm creation for ec2/main.tf
@@ -1,20 +1,57 @@
-provider "aws" {
-region = "eu-west-1"
+variable "instance" {
+  description = "Instance ID"
+  type        = string
 }
+variable "sns_topic_name" {
+  type        = string
+  description = "ARN of SNS topic that will be subscribed to an alarm."
+}
+
 ############### create Cloudwatch alarm ##########################
 resource "aws_cloudwatch_metric_alarm" "test" {
-  alarm_name         = format("%s-%s",var.instance,"CPU-UTILIZATION-HIGH")
+  alarm_name          = format("%s-%s", "${var.instance}", "High-CPU-Utilizaton")
   comparison_operator = "GreaterThanOrEqualToThreshold"
   evaluation_periods  = "2"
   metric_name         = "CPUUtilization"
   namespace           = "AWS/EC2"
-  period              = "180"
+  period              = "300"
   statistic           = "Average"
   threshold           = "90"
- alarm_actions        = [var.sns_topic_name]
+  alarm_actions       = [var.sns_topic_name]
 
   dimensions = {
     InstanceId = "${var.instance}"
-    TopicName = "${var.sns_topic_name}"
   }
- }
+}
+
+
+resource "aws_cloudwatch_metric_alarm" "test1" {
+  alarm_name          = format("%s-%s", "${var.instance}", "Warning-CPU-Utilizaton")
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/EC2"
+  period              = "300"
+  statistic           = "Average"
+  threshold           = "80"
+  alarm_actions       = [var.sns_topic_name]
+
+  dimensions = {
+    InstanceId = "${var.instance}"
+  }
+}
+resource "aws_cloudwatch_metric_alarm" "test2" {
+  alarm_name         = format("%s-%s",var.instance,"StatusCheckFailed")
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "StatusCheckFailed"
+  namespace           = "AWS/EC2"
+  period              = "180"
+  statistic           = "Average"
+  threshold           = "1"
+ alarm_actions        = [var.sns_topic_name]
+
+dimensions = {
+    InstanceId = "${var.instance}"
+  }
+}
