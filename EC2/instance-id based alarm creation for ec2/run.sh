e3a3b3eba35eb2aa12af2785a3bce5b51c381c1a
Nishachinnannan <100110248+Nishachinnannan@users.noreply.github.com>
2022-12-19 08:57:03 +0530

    Update run.sh

--- EC2/instance-id based alarm creation for ec2/run.sh
+++ EC2/instance-id based alarm creation for ec2/run.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-var-1=$1
-var-2=$2
+id=$1
+sns=$2
 
-echo $var-1
-echo $var-2
-
-#terraform init -input=false
-#terraform plan -input=false -var-file 'instance=${var}' -lock=false
-#terraform apply -input=false -var-file 'instance=${var}' -auto-approve
-
-
-
-
-#terraform init -input=false
-#terraform plan -input=false -var-file 'sns_topic_name=${var}' -lock=false
-#terraform apply -input=false -var-file 'sns_topic_name=${var}' -auto-approve
+terraform init -input=false
+terraform plan -input=false -var=instance="${id}" -var=sns_topic_name="${sns}" -lock=false
+terraform apply -input=false -var=instance="${id}" -var=sns_topic_name="${sns}" -auto-approve
